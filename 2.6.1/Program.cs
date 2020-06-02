479f0bc33e6f710edf5929c3939850d5fcce3a85
devhl-labs <shawnkanyer@gmail.com>
2020-06-02 17:23:47 -0400

    added 2.6

---
+++ 2.6.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string radiusPrompt = "Enter the radius of the circle.";
+
+            double radius = 0;
+
+            Console.WriteLine(radiusPrompt);
+            while (double.TryParse(Console.ReadLine(), out radius) == false)
+                Console.WriteLine(radiusPrompt);
+
+            Console.WriteLine($"The area of a circle of radius {radius} is: {Math.Round(Math.PI * radius * radius, 3)}");
+
+            Console.WriteLine($"The circumference of a circle of radius {radius} is: {Math.Round(2 * Math.PI * radius, 3)}");
+
+            Console.WriteLine($"The diameter of a circle of radius {radius} is: {Math.Round(2 * radius, 3)}");
+        }
+    }
+}
---
+++ 2.6.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2._6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string mpgPrompt = "Enter your vehicle's miles per gallon.";
+
+            double mpg;
+
+            Console.WriteLine(mpgPrompt);
+
+            while (double.TryParse(Console.ReadLine(), out mpg) == false || mpg <= 0)
+                Console.WriteLine(mpgPrompt);
+
+            const string radiusPrompt = "Enter the radius of the circle in miles.";
+
+
+            Console.WriteLine(radiusPrompt);
+
+            double radius;
+
+            while (double.TryParse(Console.ReadLine(), out radius) == false || radius <= 0)
+                Console.WriteLine(radiusPrompt);
+
+            Console.WriteLine($"You will use {Math.Round(radius / mpg, 2)} gallon to drive around the circle.");
+
+            Console.ReadLine();
+        }
+    }
+}
